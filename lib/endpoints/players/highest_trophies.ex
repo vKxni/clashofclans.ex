5f153eb610dce8dc9f4cad205f407e003b7cf4f4
Koni vKxni <eventbeate@gmail.com>
2022-10-24 21:34:03 +0200

    spec for highest trophy

--- lib/endpoints/players/highest_trophies.ex
+++ lib/endpoints/players/highest_trophies.ex
@@ -2,6 +2,7 @@ defmodule Endpoints.Players.HighestTrophies do
   alias Endpoints.Players.Raw
   alias Api.Errors.NotFound
 
+  @spec get_players_highest_trophies(String.t) :: integer()
   def get_players_highest_trophies(playertag) do
     with {:ok, account} <- Raw.get_raw_player_information(playertag) do
       if account["reason"] == "notFound" do
