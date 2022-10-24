1f3c6cbb4586fbe4df0607aa94a9a4ad4fdac0f8
Koni vKxni <eventbeate@gmail.com>
2022-10-24 18:35:43 +0200

    LL handler

--- lib/endpoints/players/legend_league/current_season.ex
+++ lib/endpoints/players/legend_league/current_season.ex
@@ -1,9 +1,14 @@
 defmodule Endpoints.Players.LegendLeague.CurrentSeason do
   alias Endpoints.Players.Raw
+  alias Api.Errors.NotFound
 
   def get_players_current_legend_season(playertag) do
     with {:ok, account} <- Raw.get_raw_player_information(playertag) do
-      Kernel.get_in(account, ["legendStatistics", "currentSeason"])
+      if account["reason"] == "notFound" do
+        NotFound.not_found(account)
+      else
+        Kernel.get_in(account, ["legendStatistics", "currentSeason"])
+      end
     end
   end
 end
--- lib/endpoints/players/legend_league/previous_season.ex
+++ lib/endpoints/players/legend_league/previous_season.ex
@@ -1,21 +1,34 @@
 defmodule Endpoints.Players.LegendLeague.PreviousSeason do
   alias Endpoints.Players.Raw
+  alias Api.Errors.NotFound
 
   def get_players_previous_legend_season_rank(playertag) do
     with {:ok, account} <- Raw.get_raw_player_information(playertag) do
-      Kernel.get_in(account, ["legendStatistics", "previousSeason", "rank"])
+      if account["reason"] == "notFound" do
+        NotFound.not_found(account)
+      else
+        Kernel.get_in(account, ["legendStatistics", "previousSeason", "rank"])
+      end
     end
   end
 
   def get_players_previous_legend_season_trophies(playertag) do
     with {:ok, account} <- Raw.get_raw_player_information(playertag) do
-      Kernel.get_in(account, ["legendStatistics", "previousSeason", "trophies"])
+      if account["reason"] == "notFound" do
+        NotFound.not_found(account)
+      else
+        Kernel.get_in(account, ["legendStatistics", "previousSeason", "trophies"])
+      end
     end
   end
 
   def get_players_previous_legend_season_id(playertag) do
     with {:ok, account} <- Raw.get_raw_player_information(playertag) do
-      Kernel.get_in(account, ["legendStatistics", "previousSeason", "id"])
+      if account["reason"] == "notFound" do
+        NotFound.not_found(account)
+      else
+        Kernel.get_in(account, ["legendStatistics", "previousSeason", "id"])
+      end
     end
   end
 end
