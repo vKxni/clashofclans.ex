3fa4294410a704a4ff0f97eb546730319a87a791
Koni vKxni <eventbeate@gmail.com>
2022-10-25 20:34:31 +0200

    fix LLTbug

--- lib/endpoints/players/legend_league/legend_trophies.ex
+++ lib/endpoints/players/legend_league/legend_trophies.ex
@@ -3,11 +3,7 @@ defmodule Endpoints.Players.LegendLeague.LegendTrophies do
 
   def get_players_legend_trophies(playertag) do
     with {:ok, account} <- Raw.get_raw_player_information(playertag) do
-      if account["reason"] == nil do
-        {:error, "Player is invalid, banned or hasn't been to Legends League yet. "}
-      else
-        Kernel.get_in(account, ["legendStatistics", "legendTrophies"])
-      end
+      Kernel.get_in(account, ["legendStatistics", "legendTrophies"])
     end
   end
 end
