2f408f815e0eb39aaacd46d7188dd7f350fcb9cd
Koni vKxni <eventbeate@gmail.com>
2022-10-26 21:40:24 +0200

    fixed league bug, temporarily

--- lib/endpoints/players/league.ex
+++ lib/endpoints/players/league.ex
@@ -3,11 +3,7 @@ defmodule Endpoints.Players.League do
 
   def get_league_of_player(playertag) do
     with {:ok, account} <- Raw.get_raw_player_information(playertag) do
-      if account["reason"] == nil do
-        {:error, "Player is invalid, banned or hasn't been to a league yet. "}
-      else
       Kernel.get_in(account, ["league", "name"])
     end
   end
-end
 end
